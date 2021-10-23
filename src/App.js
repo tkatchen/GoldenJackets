da169b5bbeef6e04ea83342535a54e39e1067fb3
AbhinavB27 <45719127+AbhinavB27@users.noreply.github.com>
2021-10-23 04:10:04 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Services from './Pages/Services';
+import Products from './Pages/Products';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,36 +1,46 @@
-import React, {Component} from 'react';
-import {MenuItems} from "./MenuItems.js";
-import { Button } from '../Button.js';
+import React, { useState } from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import { SidebarData } from './SidebarData';
 import './Navbar.css';
+import { IconContext } from 'react-icons';
 
-class Navbar extends Component {
-    state = {clicked: false}
-    handleClick = () => {
-        this.setState({clicked: !this.state.clicked})
-    }
-    render() {
-        return(
-            <nav className="NavbarItems">
-                <h1 className="Navbar-logo">Golden Jackets<i className="fab fa-react"></i></h1>
-                <div className="menu-icon" onClick={this.handleClick}>
-                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-                </div>
-                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-                    {MenuItems.map((item,index) => {
-                        return(
-                            <li key={index}>
-                                <a className={item.cName} href={item.url}>
-                                    {item.title}
-                                </a>    
-                            </li>
-                        )
-                    })}
-                </ul>
-                <Button>Sign Up/Login</Button>
-            </nav>
-        )
-    }
-    
+function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <div className='navbar'>
+          <Link to='#' className='menu-bars'>
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>
+        </div>
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+          <ul className='nav-menu-items' onClick={showSidebar}>
+            <li className='navbar-toggle'>
+              <Link to='#' className='menu-bars'>
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
