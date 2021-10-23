67cafd9e18922e067c8996d6e0a3ebd439bbe9a4
AbhinavB27 <45719127+AbhinavB27@users.noreply.github.com>
2021-10-23 06:46:14 -0400

    bullshit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import './Home.css';
+import logo from '../Components/Navbar/GeorgiaTechlogo.png';
+console.log(logo);
 function Home() {
   return (
     <div className='home'>
-      <h1>Home</h1>
+      <h1 className = 'titletext'>
+          Welcome to Golden Jackets! {"\n"}
+          A pawn shop meant for GaTech Students.
+      </h1>
+      <img src={logo} className='logo'></img>
+      
     </div>
   );
 }
