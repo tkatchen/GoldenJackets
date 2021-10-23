01656de982c20d02c43a12b659d4b0233e5a5fae
AbhinavB27 <45719127+AbhinavB27@users.noreply.github.com>
2021-10-23 15:29:26 -0400

    changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,16 +2,25 @@ import React from 'react';
 import './Home.css';
 import logo from '../Components/Navbar/GeorgiaTechlogo.png';
 console.log(logo);
+import Navbar from '../Components/Navbar/Navbar';
 function Home() {
   return (
-    <div className='home'>
-      <h1 className = 'titletext'>
-          Welcome to Golden Jackets! {"\n"}
-          A pawn shop meant for GaTech Students.
-      </h1>
-      <img src={logo} className='logo'></img>
-      
+    
+    <div>
+        <div className='home'>
+        <div className='wrapper'>
+            <div className='banner-text'>
+                <div className='text-area'>
+                    <h2>Welcome to Golden Jackets!</h2>
+                    <h3>A pawn shop for GaTech Students.</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+
+            </div>
+        </div>
     </div>
+    </div>
+    
   );
 }
 
