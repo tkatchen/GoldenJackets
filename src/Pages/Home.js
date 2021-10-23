56f66cc106387a948b4ed984e3872ceb7ccb47c5
tklaxbro <tkatchen7@gmail.com>
2021-10-23 15:39:57 -0400

    fix bg

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,17 +8,16 @@ function Home() {
     
     <div>
         <div className='home'>
-        <div className='wrapper'>
+          <div className='wrapper'>
             <div className='banner-text'>
                 <div className='text-area'>
                     <h2>Welcome to Golden Jackets!</h2>
                     <h3>A pawn shop for GaTech Students.</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
                 </div>
-
             </div>
         </div>
-    </div>
+      </div>
     </div>
     
   );
