eba770912ac759117b16909d01514f40f8bbcc4c
tklaxbro <tkatchen7@gmail.com>
2021-10-23 19:39:06 -0400

    Make product data actual

--- src/Pages/ProductData.js
+++ src/Pages/ProductData.js
@@ -1,8 +1,8 @@
 const product_card = [
     {
         id: 1,
-        product_name: "Item 1",
-        description: "Gold Coins",
+        product_name: "1/10oz Gold Coin",
+        description: "999 Fine Gold Coin",
         price: 200,
         currency: "$",
         thumb: "../Images/1_10OZ_GOLD_COINS.jpg"
@@ -10,63 +10,63 @@ const product_card = [
     },
     {
         id: 2,
-        product_name: "Item 2",
-        description: "Wedding Band",
-        price: 200,
+        product_name: "Wedding Band",
+        description: "14k Gold 6mm Wedding Band",
+        price: 250,
         currency: "$",
         thumb: "../Images/14K_6MM_WEDDING_BAND.jpg"
 
     },
     {
         id: 3,
-        product_name: "Item 3",
-        description: "Gold Label Pin",
-        price: 200,
+        product_name: "Gold Crest Pin",
+        description: "14k Gold Tech Crest Pin",
+        price: 175,
         currency: "$",
         thumb: "../Images/14K_GOLD_LABEL_PIN.jpg"
 
     },
     {
         id: 4,
-        product_name: "Item 4",
+        product_name: "Poker Set",
         description: "500 Piece Poker Set",
-        price: 200,
+        price: 65,
         currency: "$",
         thumb: "../Images/500_PIECE_POKER_SET.jpeg"
 
     },
     {
         id: 5,
-        product_name: "Item 5",
-        description: "NCAA Championship Ring",
-        price: 200,
+        product_name: "NCAA Championship Ring",
+        description: "1990 NCAA Footbal Championship Ring",
+        price: 450,
         currency: "$",
         thumb: "../Images/1990_NCAA_CHAMPIONSHIP.jpeg"
 
     },
     {
         id: 6,
-        product_name: "Item 6",
-        description: "Game Worn World Series Jersy",
-        price: 200,
+        product_name: "NCAA World Series Jersy",
+        description: "1994 NCAA World Series Game Worn Signed Jersy",
+        price: 700,
         currency: "$",
         thumb: "../Images/1994_GAME_WORN_SIGNED_COLLEGE_WORLD_SERIES.jpg"
 
     },
     {
         id: 7,
-        product_name: "Item 7",
-        description: "Championship Ring",
-        price: 200,
+        product_name: "Orange Bowl Ring",
+        description: "2014 Orange Bowl Championship Ring",
+        price: 320,
         currency: "$",
         thumb: "../Images/ORANGE_BOWL_CHAMPIONS.jpg"
 
     },
     {
         id: 8,
-        product_name: "Item 8",
-        description: "Silver Coins",
-        price: 200,
+        product_name: "1oz Silver Coin",
+        description: "999 Fine Silver Coin",
+        price: 25,
         currency: "$",
         thumb: "../Images/SILVER_COINS.jpg"
 
