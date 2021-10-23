5fbd2c1d6110b8d50a8802b9401eaac136c93214
tklaxbro <tkatchen7@gmail.com>
2021-10-23 15:28:30 -0400

    addCards now array

--- Backend/express.js
+++ Backend/express.js
@@ -19,15 +19,16 @@ app.post("/login", async (req, res) => {
 })
 
 app.get("/getCards", async (req, res) => {
-    let cards = await mysql.getCards()
-    res.send(Array.from(cards[0].cards.data))
+    res.send(await mysql.getCards())
 })
 
-app.post("/addCard", async (req, res) => {
-    let cards = await mysql.getCards()
-    cards.push(req.query.card)
-    await mysql.updateCards(cards)
-    res.send(cards)
+app.post("/addCards", (req, res) => {
+    mysql.getCards().then(async p => {
+        cards = [...p]
+        cards.push(...req.query.cards)
+        await mysql.updateCards(cards)
+        res.send(cards)
+    })
 })
 
 app.listen(6969)
--- Backend/mysql.js
+++ Backend/mysql.js
@@ -24,7 +24,7 @@ module.exports.MySQL = class MySQL {
     register(username, pass) {
         return new Promise((resolve, reject) => {
             this.connection.query(`INSERT INTO users (username, pass, cards) \
-            values ("${username}", "${pass}", \"0x5b 0x5d\");`,
+            values ("${username}", "${pass}", "");`,
             (error, results, fields) => {
                 if(error) resolve(false)
                 this.username = username
@@ -55,7 +55,8 @@ module.exports.MySQL = class MySQL {
             (error, results, fields) => {
                 if(error) throw error
                 // Figuring this one out was a blast...
-                resolve(JSON.parse(Buffer.from(Buffer.from(JSON.parse(JSON.stringify(results))[0].cards.data).toString("utf8").match(/.{2}/g).map(x => "0x"+x), "hex").toString("utf8")))
+                if(!Buffer.from(JSON.parse(JSON.stringify(results))[0].cards.data).toString("utf8")) return resolve([])
+                resolve(JSON.parse(JSON.parse(JSON.stringify(Buffer.from(Buffer.from(JSON.parse(JSON.stringify(results))[0].cards.data).toString("utf8").match(/.{2}/g).map(x => "0x"+x), "hex").toString("utf8")).replace(/\u0000/g,""))).join("").match(/.{2}/g))
             })
         })
     }
